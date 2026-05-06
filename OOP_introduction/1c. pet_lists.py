2cc83af5bdbd4bd993213d5405bc2a92533d7431
BowenGurney <bowen.gurney@education.nsw.gov.au>
2026-05-06 23:39:18 +0000

    Unfinished 1c

--- OOP_introduction/1c. pet_lists.py
+++ OOP_introduction/1c. pet_lists.py
@@ -1,5 +1,11 @@
 #Tutorial 3 Lists:
 #1. Create an example of parallel lists eg: pet_name, species, age, vaccination_status for three pets
+
+pet_name = ['Dork',  'Ponyo', 'Princess Mononoke', 'Prince Ashitaka']
+species = ['Chihuahua', 'Pomeranian', 'Yorkshire Terrier', 'Maltese', 'Papillon', 'Shih Tzu', 'Pug']
+age = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 120]
+vaccination_status =  [False, True]
+
 #2. Use a for loop to print parallel list details. This will mean that one complete printout will look like:
 '''
 Pet name: Foxy
@@ -7,6 +13,12 @@ Species: Dog
 Age: 8
 Vaccination Status: False
 '''
+
+for i in range(1):
+    print(pet_name)
+    print(species)
+    print(age)
+    print(vaccination_status)
 #3. Demonstrate what happens when an item is deleted
 
 
