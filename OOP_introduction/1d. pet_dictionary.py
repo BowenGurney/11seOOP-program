623451a1e431076b9f3c7241b61ff62a303a6eba
BowenGurney <bowen.gurney@education.nsw.gov.au>
2026-05-08 04:38:38 +0000

    1d. Doing dictionary

--- OOP_introduction/1d. pet_dictionary.py
+++ OOP_introduction/1d. pet_dictionary.py
@@ -5,13 +5,46 @@
 
 pet1 = {
 'name' : 'Bonnie',
-'animal category' : 'Cat',
+'animal category' : 'Snapping turtle',
 'age' : 3,
 'vaccinated' : True,
 'credit card' : '3423 2326 7543 1234',
 'billing address' : '17 Park Drive, The Shire 3695',
 'owner name' : 'Annie Jenkins',
 'account balance' : 129.95,
+}
+
+pet2 = {
+'name' : 'Jack',
+'animal category' : 'Dog',
+'age' : 7,
+'vaccinated' : True,
+'credit card' : '4539 0184 2930 4712',
+'billing address' : '16 Ur Mom Drive, Mount Druitt 2775',
+'owner name' : 'Jack Jack',
+'account balance' : 86680.53,
+}
+
+pet3 = {
+'name' : 'Ponyo',
+'animal category' : 'Snake',
+'age' : 6,
+'vaccinated' : False,
+'credit card' : '5241 8203 9481 0563',
+'billing address' : '88 Australia Rd, Bidwill 2999',
+'owner name' : 'Xian Li',
+'account balance' : 98800923.36,
+}
+
+pet4 = {
+'name' : 'Mononoke',
+'animal category' : 'Cat',
+'age' : 67,
+'vaccinated' : True,
+'credit card' : '3782 8224 6310 0058',
+'billing address' : '125 Melbourne Rd, Willmot 2121',
+'owner name' : 'Ali Hini',
+'account balance' : 11.07,
 }
 
 
