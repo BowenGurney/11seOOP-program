fd03d6e021531cb0c1859c19efbb491aaa90e72b
BowenGurney <bowen.gurney@education.nsw.gov.au>
2026-05-12 04:32:10 +0000

    Finishing 3d

--- OOP_introduction/3d. car_class_activity.py
+++ OOP_introduction/3d. car_class_activity.py
@@ -1,26 +1,29 @@
-# Learning intentions:
+    # Learning intentions:
 # - Create a car class example
 # - Use attributes: make, model, year and price
 # - Create a __str__ method that prints make and model
 
 class Car:
-    def __init__(self,make,model,year,price=None):
+    def __init__(self,make,model,year,price=None,sale=False):
         self.make = make
         self.model = model
         self.year = year
         self.price = price
+        self.for_sale = sale
 
     def __str__(self):
-
+        possible_cars = 'Brand: ' + self.make + '\n Model: ' + self.model + '\n Year: ' + str(self.year) + '\n Price: ' + str(self.price) + '\n Sale: ' + str(self.for_sale)
+        return possible_cars
 
 c1 = Car('Mazda','6',2005)
+c2 = Car('Kia', '3', 2009,'$1249.99',True)
+c3 = Car('Ferrari','1',2011,'$600,000.00')
+c4 = Car('Lamborgini', '23',2004,'$900,000',True)
 
-
-cars = [c1]
+cars = [c1, c2, c3, c4]
 
 for car in cars:
     print(car)
-
 
 #ACTIVITIES:
 #1. Istantiate another car object
